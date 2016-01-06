47f791a0edc13cf1fd28d9eafac725fe4f08faa1
Simon Kühling <mail@simonkuehling.de>
2016-01-06 14:52:44 +0100

    update firmware version string

--- src/ArduinoAVR/Repetier/Repetier.h
+++ src/ArduinoAVR/Repetier/Repetier.h
@@ -23,7 +23,7 @@
 #define _REPETIER_H
 
 #define REPETIER_VERSION "0.91"
-#define REPRAPINDUSTRIAL_FIRMWARE_VERSION "0.91-rri1.3.0-01-e400"
+#define REPRAPINDUSTRIAL_FIRMWARE_VERSION "0.91-rri1.3.3-01-e400"
 
 // ##########################################################################################
 // ##                                  Debug configuration                                 ##
