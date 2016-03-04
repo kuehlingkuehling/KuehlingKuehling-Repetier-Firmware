bb9355a072cf43bdc9cbbfdfbf5b2d966d498543
Simon Kühling <mail@simonkuehling.de>
2016-03-04 14:22:10 +0100

    tweak SPI clock rate for MAX31855 thermocouple chips

--- src/ArduinoDUE/Repetier/Extruder.cpp
+++ src/ArduinoDUE/Repetier/Extruder.cpp
@@ -1966,7 +1966,7 @@ int16_t read_max31855(uint8_t ss_pin)
 {
     uint32_t data = 0;
     int16_t temperature;
-    HAL::spiInit(2);
+    HAL::spiInit(1);
     HAL::digitalWrite(ss_pin, 0);  // enable TT_MAX31855
     HAL::delayMicroseconds(1);    // ensure 100ns delay - a bit extra is fine
 
