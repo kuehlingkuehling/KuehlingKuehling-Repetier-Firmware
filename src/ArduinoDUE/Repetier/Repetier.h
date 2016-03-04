2d48ba00b098fce81c656229bf9eaedcf7c5fdd4
Simon Kühling <mail@simonkuehling.de>
2016-03-04 14:21:07 +0100

    add Kühling&Kühling Firmware version to M115 capabilities string

--- src/ArduinoDUE/Repetier/Repetier.h
+++ src/ArduinoDUE/Repetier/Repetier.h
@@ -25,6 +25,7 @@
 #include <math.h>
 #include <stdint.h>
 #define REPETIER_VERSION "0.92.8"
+#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-ht500-1.4.0-01"
 
 // ##########################################################################################
 // ##                                  Debug configuration                                 ##
