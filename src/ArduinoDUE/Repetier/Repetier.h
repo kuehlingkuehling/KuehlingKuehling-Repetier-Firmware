27e2e632fc63ff048d68129a64686a2b07fbbb04
Simon Kühling <mail@simonkuehling.de>
2016-04-21 10:03:15 +0200

    update firmware version string

--- src/ArduinoDUE/Repetier/Repetier.h
+++ src/ArduinoDUE/Repetier/Repetier.h
@@ -25,7 +25,7 @@
 #include <math.h>
 #include <stdint.h>
 #define REPETIER_VERSION "0.92.8"
-#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-ht500-1.4.1-01"
+#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-ht500-1.4.2-01"
 
 // ##########################################################################################
 // ##                                  Debug configuration                                 ##
