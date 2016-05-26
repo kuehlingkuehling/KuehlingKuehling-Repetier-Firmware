a66424f69c4660822568aa9ec995df54c58d1d52
Simon Kühling <mail@simonkuehling.de>
2016-05-26 11:54:45 +0200

    introduce FEATURE_DECOUPLE_TEST switch to disable decoupling logic

--- src/ArduinoDUE/Repetier/Configuration.h
+++ src/ArduinoDUE/Repetier/Configuration.h
@@ -62,6 +62,12 @@
 #define BLUETOOTH_BAUD  115200
 #define MIXING_EXTRUDER 0
 
+// manually added switch to disable decoupling logic
+//   as it does not play well with the heated chamber driven by the 3rd extruder
+//   (opening the door triggers the decoupling error - chamber temp sensor and chamber heaters
+//   are decoupled by design, after all)
+#define FEATURE_DECOUPLE_TEST 0      
+
 #define DRIVE_SYSTEM 3
 #define XAXIS_STEPS_PER_MM 256
 #define YAXIS_STEPS_PER_MM 256
