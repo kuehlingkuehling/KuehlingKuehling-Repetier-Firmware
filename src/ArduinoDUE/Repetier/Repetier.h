0611b1aa279a2e72dd0e117308b7e1805d813b00
Simon Kühling <mail@simonkuehling.de>
2017-06-19 18:09:52 +0200

    release 0.92-vp75-1.1.0-02

--- src/ArduinoDUE/Repetier/Repetier.h
+++ src/ArduinoDUE/Repetier/Repetier.h
@@ -25,7 +25,7 @@
 #include <math.h>
 #include <stdint.h>
 #define REPETIER_VERSION "0.92.8"
-#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-vp75-1.1.0-01"
+#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-vp75-1.1.0-02"
 
 // ##########################################################################################
 // ##                                  Debug configuration                                 ##
