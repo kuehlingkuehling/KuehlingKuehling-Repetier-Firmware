3b72d6aa6baedd85424d1656c5f96d522ad990e4
Simon Kühling <mail@simonkuehling.de>
2017-07-10 15:06:45 +0200

    release 0.92-ht500-1.4.4-02

--- src/ArduinoDUE/Repetier/Repetier.h
+++ src/ArduinoDUE/Repetier/Repetier.h
@@ -25,7 +25,7 @@
 #include <math.h>
 #include <stdint.h>
 #define REPETIER_VERSION "0.92.8"
-#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-ht500-1.4.4-01"
+#define KUEHLINGKUEHLING_FIRMWARE_VERSION "0.92-ht500-1.4.4-02"
 
 // ##########################################################################################
 // ##                                  Debug configuration                                 ##
